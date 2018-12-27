e75d034d6f112226afd87b1b85f61706cc7c1f33
Sullivanjc <sullivanjc91@gmail.com>
2018-12-27 03:48:19 -0600

    Images of dogs display in global feed

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -3,12 +3,12 @@ import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
+import FeedScreen from '../screens/FeedScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+  Home: FeedScreen,
 });
 
 HomeStack.navigationOptions = {
